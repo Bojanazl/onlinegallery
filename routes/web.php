ea5b931d1c8edc249ba2d0d067ac17deb98ea998
Bojana Jokic <bojanajokiczl@gmail.com>
2023-04-24 02:06:31 +0200

    Autherntication vews
    Creating views for register, login, dashboard, layouts

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
-Route::get('/index', function () {
+Route::get('/dashboard', function () {
     return view('welcome');
 });
 
