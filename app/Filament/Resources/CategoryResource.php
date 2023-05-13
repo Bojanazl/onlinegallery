7f2e2422bac3e86cfae9b5f32d6a5176303a517b
Bojana Jokic <bojanajokiczl@gmail.com>
2023-05-13 03:26:54 +0200

    Create Post Resource and Category Resource
    Created Post and Category Resources in Dashboard (not simple create). Delete Post and Delete Category still not working. Edit still not returning to the list.

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\CategoryResource\Pages;
 use App\Filament\Resources\CategoryResource\RelationManagers;
 use App\Models\Category;
-use Closure;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -13,7 +12,6 @@ use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Str;
 
 class CategoryResource extends Resource
 {
@@ -21,20 +19,11 @@ class CategoryResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-collection';
 
-    protected static ?string $navigationGroup = 'Content';
-
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 Forms\Components\TextInput::make('title')
-                    ->required()
-                    ->maxLength(2048)
-                    ->reactive()
-                    ->afterStateUpdated(function (Closure $set, $state) {
-                        $set('slug', Str::slug($state));
-                    }),
-                Forms\Components\TextInput::make('slug')
                     ->required()
                     ->maxLength(2048),
             ]);
@@ -44,10 +33,10 @@ class CategoryResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('title')->searchable()
-                    ->sortable(),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
-                    ->sortable()
                     ->dateTime(),
             ])
             ->filters([
@@ -61,11 +50,20 @@ class CategoryResource extends Resource
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
     public static function getPages(): array
     {
         return [
-            'index' => Pages\ManageCategories::route('/'),
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
         ];
-    }
+    }    
 }
--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Post;
-use Closure;
 use Filament\Forms;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
@@ -13,65 +12,50 @@ use Filament\Resources\Table;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Str;
 
 class PostResource extends Resource
 {
     protected static ?string $model = Post::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-document-text';
-
-    protected static ?string $navigationGroup = 'Content';
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\Card::make()
-                    ->schema([
-                        Forms\Components\TextInput::make('title')
-                            ->required()
-                            ->maxLength(2048)
-                            ->reactive()
-                            ->afterStateUpdated(function (Closure $set, $state) {
-                                $set('slug', Str::slug($state));
-                            }),
-                        Forms\Components\TextInput::make('slug')
-                            ->required()
-                            ->maxLength(2048),
-                        Forms\Components\Textarea::make('body')
-                            ->required(),
-                        Forms\Components\TextInput::make('meta_title')
-                            ->maxLength(255),
-                        Forms\Components\Textarea::make('meta_description')
-                            ->maxLength(255),
-                        Forms\Components\Checkbox::make('active')
-                            ->required(),
-                        Forms\Components\DateTimePicker::make('published_at')
-                            ->placeholder('d/m/y' )
-                    ])->columnSpan(7),
-
-                Forms\Components\Card::make()
-                    ->schema([
-                        Forms\Components\FileUpload::make('thumbnail'),
-                        Forms\Components\Select::make('categories')
-                            ->multiple()
-                            ->relationship('categories', 'title'),
-                    ])->columnSpan(5)
-            ])->columns(12);
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(2048),
+                Forms\Components\TextInput::make('slug')
+                    ->required()
+                    ->maxLength(2048),
+                Forms\Components\TextInput::make('thumbnail')
+                    ->maxLength(2048),
+                Forms\Components\Textarea::make('body')
+                    ->required(),
+                Forms\Components\Checkbox::make('active')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('published_at'),
+                Forms\Components\Select::make('user_id')
+                    ->relationship('user', 'name')
+                    ->required(),
+            ]);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\ImageColumn::make('thumbnail'),
-                Tables\Columns\TextColumn::make('title')->searchable(['title', 'body'])->sortable(),
+                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('slug'),
+                Tables\Columns\TextColumn::make('thumbnail'),
+                Tables\Columns\TextColumn::make('body'),
                 Tables\Columns\IconColumn::make('active')
-                    ->sortable()
                     ->boolean(),
                 Tables\Columns\TextColumn::make('published_at')
-                    ->sortable()
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('user.name'),
+                Tables\Columns\TextColumn::make('created_at')
                     ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime(),
@@ -82,20 +66,19 @@ class PostResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-
+    
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-
+    
     public static function getPages(): array
     {
         return [
@@ -104,5 +87,5 @@ class PostResource extends Resource
             'view' => Pages\ViewPost::route('/{record}'),
             'edit' => Pages\EditPost::route('/{record}/edit'),
         ];
-    }
+    }    
 }
