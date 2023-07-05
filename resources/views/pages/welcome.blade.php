419985470faf4da29961c4c314a4558cc36e3ac4
Bojana Jokic <bojanajokiczl@gmail.com>
2023-07-05 11:37:05 +0100

    Fixed css inclusion and font color
    Created new folder in public folder named css where css files can be included from into blades.
    Changed nav font color.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,213 +2,16 @@
 <html>
  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
-<style>
-  body, html {
-    height: 100%;
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
-  }
-  
-  .hero-image {
-    background-image: url("/images/header.png");
-    height: 50%;
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
-    position: relative;
-  }
-  
-  .hero-text {
-    text-align: left;
-    position: absolute;
-    top: 10%;
-    left: 10%;
-    transform: translate(-50%, -50%);
-    color: rgba(87, 190, 211, 0.626);
-  }
-  
-  .hero-text-subbtitle {
-    text-align: left;
-    position: absolute;
-    top: 20%;
-    left: 20%;
-    transform: translate(-50%, -50%);
-    color: rgba(87, 190, 211, 0.426);
-  }
-  
-  .hero-text button {
-    border: none;
-    outline: 0;
-    display: inline-block;
-    padding: 10px 25px;
-    color: rgba(34, 98, 111, 0.678);
-    background-color: #ffffff;
-    text-align: left;
-    cursor: pointer;
-  }
-  
-  .hero-text button:hover {
-    background-color: #555;
-    color: rgb(56, 85, 109);
-  }
-  
-  .top-container {
-    background-color: #020812;
-    padding: 30px;
-    text-align: center;
-  }
-  
-  .header {
-    padding: 10px 10px;
-    background: #001b28dc;
-    color: rgba(87, 190, 211, 0.626);
-    padding: 10px;
-  }
-  
-  .content {
-    padding: 16px;
-  }
-  
-  .sticky {
-    position: fixed;
-    top: 0;
-    width: 100%;
-  }
-  
-  .sticky + .content {
-    padding-top: 102px;
-  }
 
-  .tabcontent {
-    color: white;
-    display: none;
-    padding: 20px;
-    text-align: center;
-  }
-
-  .navbar {
-  overflow: hidden;
-  background-color: #04113900;
-}
-
-.navbar a {
-  float: left;
-  font-size: 16px;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-
-.navbar a:hover, .dropdown:hover .dropbtn {
-  background-color: rgba(87, 190, 211, 0.626);
-}
-
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Float four columns side by side */
-.column {
-  float: left;
-  width: 25%;
-  padding: 0 10px;
-}
-
-/* Remove extra left and right margins, due to padding */
-.row {margin: 0 -5px;}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Responsive columns */
-@media screen and (max-width: 600px) {
-  .column {
-    width: 100%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
-
-/* Style the counter cards */
-.card {
-  box-shadow: 0 4px 8px 0 rgba(1, 38, 26, 0.2);
-  padding: 15px;
-  text-align: center;
-  background-color: rgba(48, 175, 201, 0.141);
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.card-inner {
-  padding: 15px;
-  box-shadow: 0 4px 8px 0 rgba(1, 2, 16, 0.508);
-  text-align: center;
-  background-color: #020812;
-  border-color: rgb(13, 108, 128);
-  border-bottom: 3px solid
-}
-
-.font-aclonica {
-  font-family: 'Aclonica';
-}
-
-
-.card button {
-  margin-top: 20px;
-  border: none;
-  outline: 0;
-  padding: 12px;
-  color:  rgb(20, 152, 179);
-  background-color: rgba(48, 175, 201, 0.275);
-  text-align: center;
-  cursor: pointer;
-  width: 100%;
-  font-size: 18px;
-}
-
-.card button:hover {
-  opacity: 0.7;
-}
-
-.font-color {
-  color: rgb(20, 152, 179);
-}
-
-.footer {
-  background-image:url("/images/footer.png");;
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-   background-color: rgba(255, 0, 0, 0);
-   color: rgba(10, 119, 120, 0.386);
-   text-align: center;
-}
-  
-</style>
-
-
-
-
+ <link rel="stylesheet" href="/css/welcome_page.css">
 
 
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="/resources/css/onlinegallery.css">
 </head>
 
 
 <body style="background-color:#020812;"> <!--#020812-->
-
  
   <!--Top navigation bar-->
 <div class="header" id="myHeader">
@@ -268,7 +71,7 @@ body {
   <div class="card">
     <div class="card-inner">
       <img src="/images/melting.jpg" alt="blog" style="width:100%">
-      <h1 class="font-color font-aclonica">Art blog</h1>
+      <h1 class="font-color font-aclonica"><a href="/blog">Art blog</a></h1>
       <p class="font-color">All about art and artistic life: news, interesting stuff, etc.</p>
       <p><a href="/blog"> <button type="button" class="btn btn-outline-primary float-right">Lets Read</button></a></p>
      
